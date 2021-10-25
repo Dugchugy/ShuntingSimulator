dcd3df5fbd28b69c6a28363b31a4f77f0bb551a1
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2021-10-24 20:19:05 -0700

    updated the scene and began work on the track builder

--- Assets/Scripts/ImprovedTrack.cs
+++ Assets/Scripts/ImprovedTrack.cs
@@ -6,11 +6,20 @@ public class ImprovedTrack : MonoBehaviour
 {
     public Vector3[] points;
 
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
+        MeshFilter meshF = GetComponent<MeshFilter>();
+
+        Mesh cloneMesh = new Mesh();
+
+
+
+        for(int i = 0; i < 11; i++){
+
+        }
 
     }
 
@@ -18,5 +27,26 @@ public class ImprovedTrack : MonoBehaviour
     void Update()
     {
 
+    }
+
+    Vector3 FindPoint(Vector3[] pts, float t){
+
+        //ensures make sure theres always multiple items in the array
+        if(pts.Length == 1){
+            return(pts[0]);
+        }
+
+        //creates an array to store the arrays outputs;
+        Vector3[] outpts = new Vector3[pts.Length - 1];
+
+        //loops through the array to excluding the last value and interpolates between the values
+        for(int i = 0; i < pts.Length - 1; i++){
+
+            //adds the interpolation between values to the outpts;
+            outpts[i] = Vector3.Lerp(pts[i], pts[i + 1], t);
+        }
+
+        //finds the points within remaining points
+        return(FindPoint(outpts, t));
     }
 }
