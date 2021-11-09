5b0f5377658e662fa87c397db24536b546aba9a6
Dugchugy <61024715+Dugchugy@users.noreply.github.com>
2021-11-08 21:31:40 -0800

    updated rails!
    created some materials to use for the rails and finished the rail generation algorithm

--- Assets/Scripts/ImprovedTrack.cs
+++ Assets/Scripts/ImprovedTrack.cs
@@ -5,6 +5,9 @@ using System;
 
 public class ImprovedTrack : MonoBehaviour
 {
+    [SerializeField] private Material TIEMAT;
+    [SerializeField] private Material RAILMAT;
+
     public const int PERECISION = 100;
 
     public Vector3[] points;
@@ -22,23 +25,226 @@ public class ImprovedTrack : MonoBehaviour
 
         Length = readLength(PERECISION);
 
+        cloneMesh.subMeshCount = 2;
+
         cloneMesh.name = "Track";
 
         meshF.mesh = cloneMesh;
 
         int lenth = (int) (Length);
 
-        for(int i = 0; i < lenth; i++){
-            float t = FindTLength((float) (i), PERECISION);
+        float offset = (Length - lenth) / 2.0f;
 
-            CreateRect(FindPoint(points, t), cloneMesh, new Vector3(0.1f, 0.1f, 0.5f), FindDir(points, t));
+        CreateRailEnds(FindPoint(points, 0), cloneMesh, new Vector2(0.2f, 0.1f), FindDir(points, 0), new Vector2(0.1f, 0.7f));
+
+        for(int i = 0; i <= lenth; i++){
+            float t = FindTLength(offset + (float) (i), PERECISION);
+
+            float t2;
+
+            if(i == 0){
+                t2 = 0.0f;
+            }else{
+                t2 = FindTLength(offset + (float) (i - 1), PERECISION);
+            }
+
+            CreateRailSection(t2, t, cloneMesh, new Vector2(0.2f, 0.1f), new Vector2(0.1f, 0.7f));
+
+            CreateRect(FindPoint(points, t), cloneMesh, new Vector3(0.1f, 0.1f, 1.0f), FindDir(points, t));
         }
 
+        CreateRailSection(FindTLength(offset + lenth, PERECISION), 1.0f, cloneMesh, new Vector2(0.2f, 0.1f), new Vector2(0.1f, 0.7f));
+
+        CreateRailEnds(FindPoint(points, 1), cloneMesh, new Vector2(0.2f, 0.1f), FindDir(points, 1), new Vector2(0.1f, 0.7f));
+
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+
+        mr.materials = new Material[2] {TIEMAT, RAILMAT};
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    void CreateRailEnds(Vector3 pos, Mesh Fmesh, Vector2 size, Vector3 x, Vector2 offset){
+        //defines y as the vertical direction (positive up)
+        Vector3 y = Vector3.up;
+
+        //finds z based off of x and y
+        Vector3 z = Vector3.Cross(x, y).normalized;
+
+        for(int i = 0; i < 2; i++){
+            //1
+            List<Vector3> newVertices = new List<Vector3>(Fmesh.vertices);
+            List<Vector2> newUVs = new List<Vector2>(Fmesh.uv);
+            List<int> newTriangles = new List<int>(Fmesh.GetTriangles(1));
+
+            int addval = Fmesh.vertices.Length;
+
+            Vector3 vert1 = new Vector3(0, 0, 0);
+            Vector3 vert2 = new Vector3(0, 0, 0);
+            Vector3 vert3 = new Vector3(0, 0, 0);
+            Vector3 vert4 = new Vector3(0, 0, 0);
+
+            switch(i){
+            case 0:
+                vert1 = y * size.x + y * offset.x + z * size.y + z * offset.y + pos;
+                vert2 = y * offset.x + z * size.y + z * offset.y + pos;
+                vert3 = y * offset.x - z * size.y + z * offset.y + pos;
+                vert4 = y * size.x + y * offset.x - z * size.y + z * offset.y + pos;
+                break;
+            case 1:
+                vert1 = y * size.x + y * offset.x + z * size.y - z * offset.y + pos;
+                vert2 = y * offset.x + z * size.y - z * offset.y + pos;
+                vert3 = y * offset.x - z * size.y - z * offset.y + pos;
+                vert4 = y * size.x + y * offset.x - z * size.y - z * offset.y + pos;
+                break;
+            }
+
+            //2
+            newVertices.Add(vert1);
+            newVertices.Add(vert2);
+            newVertices.Add(vert3);
+            newVertices.Add(vert4);
+
+            //3
+            newUVs.Add(new Vector2(1, 0));
+            newUVs.Add(new Vector2(1, 1));
+            newUVs.Add(new Vector2(0, 1));
+            newUVs.Add(new Vector2(0, 0));
+
+            //4
+            newTriangles.Add(2 + addval);
+            newTriangles.Add(1 + addval);
+            newTriangles.Add(0 + addval);
+
+            //5
+            newTriangles.Add(3 + addval);
+            newTriangles.Add(2 + addval);
+            newTriangles.Add(0 + addval);
+
+            Fmesh.vertices = newVertices.ToArray();
+            Fmesh.uv = newUVs.ToArray();
+            Fmesh.SetTriangles(newTriangles.ToArray(), 1);
+        }
+    }
+
+    void CreateRailSection(float t1, float t2, Mesh Fmesh, Vector2 size, Vector2 offset){
+
+        Vector3 pos1 = FindPoint(points, t1);
+        Vector3 x1 = FindDir(points, t1);
+
+        Vector3 pos2 = FindPoint(points, t2);
+        Vector3 x2 = FindDir(points, t2);
+
+        Vector3 y = Vector3.up;
+
+        Vector3 z1 = Vector3.Cross(x1, y).normalized;
+        Vector3 z2 = Vector3.Cross(x2, y).normalized;
+
+        for(int i = 0; i < 8; i++){
+            //1
+            List<Vector3> newVertices = new List<Vector3>(Fmesh.vertices);
+            List<Vector2> newUVs = new List<Vector2>(Fmesh.uv);
+            List<int> newTriangles = new List<int>(Fmesh.GetTriangles(1));
+
+            int addval = Fmesh.vertices.Length;
+
+            Vector3 vert1 = new Vector3(0, 0, 0);
+            Vector3 vert2 = new Vector3(0, 0, 0);
+            Vector3 vert3 = new Vector3(0, 0, 0);
+            Vector3 vert4 = new Vector3(0, 0, 0);
+
+            switch(i){
+             //creates squares for each side of the rectangualr prism
+
+            case 0:
+            //creates a square for the "front" of the prism
+
+                vert4 = y * offset.x + z1 * size.y + z1 * offset.y + pos1;
+                vert3 = y * offset.x + z2 * size.y + z2 * offset.y + pos2;
+                vert2 = y * size.x + y * offset.x + z2 * size.y + z2 * offset.y + pos2;
+                vert1 = y * size.x + y * offset.x + z1 * size.y + z1 * offset.y + pos1;
+                break;
+
+
+            case 1:
+                vert1 = y * offset.x - z1 * size.y + z1 * offset.y + pos1;
+                vert2 = y * offset.x - z2 * size.y + z2 * offset.y + pos2;
+                vert3 = y * size.x + y * offset.x - z2 * size.y + z2 * offset.y + pos2;
+                vert4 = y * size.x + y * offset.x - z1 * size.y + z1 * offset.y + pos1;
+                break;
+            case 2:
+                vert4 = y * size.x + y * offset.x + z1 * size.y + z1 * offset.y + pos1;
+                vert3 = y * size.x + y * offset.x + z2 * size.y + z2 * offset.y + pos2;
+                vert2 = y * size.x + y * offset.x - z2 * size.y + z2 * offset.y + pos2;
+                vert1 = y * size.x + y * offset.x - z1 * size.y + z1 * offset.y + pos1;
+                break;
+            case 3:
+                vert1 = y * offset.x + z1 * size.y + z1 * offset.y + pos1;
+                vert2 = y * offset.x + z2 * size.y + z2 * offset.y + pos2;
+                vert3 = y * offset.x - z2 * size.y + z2 * offset.y + pos2;
+                vert4 = y * offset.x - z1 * size.y + z1 * offset.y + pos1;
+                break;
+
+            case 4:
+            //creates a square for the "front" of the prism
+
+                vert4 = y * offset.x + z1 * size.y - z1 * offset.y + pos1;
+                vert3 = y * offset.x + z2 * size.y - z2 * offset.y + pos2;
+                vert2 = y * size.x + y * offset.x + z2 * size.y - z2 * offset.y + pos2;
+                vert1 = y * size.x + y * offset.x + z1 * size.y - z1 * offset.y + pos1;
+                break;
+
+            case 5:
+                vert1 = y * offset.x - z1 * size.y - z1 * offset.y + pos1;
+                vert2 = y * offset.x - z2 * size.y - z2 * offset.y + pos2;
+                vert3 = y * size.x + y * offset.x - z2 * size.y - z2 * offset.y + pos2;
+                vert4 = y * size.x + y * offset.x - z1 * size.y - z1 * offset.y + pos1;
+                break;
+            case 6:
+                vert4 = y * size.x + y * offset.x + z1 * size.y - z1 * offset.y + pos1;
+                vert3 = y * size.x + y * offset.x + z2 * size.y - z2 * offset.y + pos2;
+                vert2 = y * size.x + y * offset.x - z2 * size.y - z2 * offset.y + pos2;
+                vert1 = y * size.x + y * offset.x - z1 * size.y - z1 * offset.y + pos1;
+                break;
+            case 7:
+                vert1 = y * offset.x + z1 * size.y - z1 * offset.y + pos1;
+                vert2 = y * offset.x + z2 * size.y - z2 * offset.y + pos2;
+                vert3 = y * offset.x - z2 * size.y - z2 * offset.y + pos2;
+                vert4 = y * offset.x - z1 * size.y - z1 * offset.y + pos1;
+                break;
+
+            }
+
+            //2
+            newVertices.Add(vert1);
+            newVertices.Add(vert2);
+            newVertices.Add(vert3);
+            newVertices.Add(vert4);
+
+            //3
+            newUVs.Add(new Vector2(1, 0));
+            newUVs.Add(new Vector2(1, 1));
+            newUVs.Add(new Vector2(0, 1));
+            newUVs.Add(new Vector2(0, 0));
+
+            //4
+            newTriangles.Add(2 + addval);
+            newTriangles.Add(1 + addval);
+            newTriangles.Add(0 + addval);
+
+            //5
+            newTriangles.Add(3 + addval);
+            newTriangles.Add(2 + addval);
+            newTriangles.Add(0 + addval);
+
+            Fmesh.vertices = newVertices.ToArray();
+            Fmesh.uv = newUVs.ToArray();
+            Fmesh.SetTriangles(newTriangles.ToArray(), 1);
+        }
 
     }
 
@@ -50,14 +256,11 @@ public class ImprovedTrack : MonoBehaviour
         //finds z based off of x and y
         Vector3 z = Vector3.Cross(x, y).normalized;
 
-        //checks the values of x, y and z:
-        Debug.LogFormat("x: {0}, y: {1}, z: {2}", x, y, z);
-
         for(int i = 0; i < 6; i++){
             //1
             List<Vector3> newVertices = new List<Vector3>(Fmesh.vertices);
             List<Vector2> newUVs = new List<Vector2>(Fmesh.uv);
-            List<int> newTriangles = new List<int>(Fmesh.triangles);
+            List<int> newTriangles = new List<int>(Fmesh.GetTriangles(0));
 
             int addval = Fmesh.vertices.Length;
 
@@ -138,7 +341,7 @@ public class ImprovedTrack : MonoBehaviour
 
             Fmesh.vertices = newVertices.ToArray();
             Fmesh.uv = newUVs.ToArray();
-            Fmesh.triangles = newTriangles.ToArray();
+            Fmesh.SetTriangles(newTriangles.ToArray(), 0);
         }
     }
 
